849d36c93e2e29f5bbfe869b4b9c3ca893bc93f0
John Starich <johnstarich@johnstarich.com>
2020-06-11 19:45:20 -0500

    Add global logLevel and setLogLevel() values

--- log/log.go
+++ log/log.go
@@ -3,49 +3,96 @@ package log
 import (
 	"fmt"
 	"syscall/js"
+
+	"github.com/johnstarich/go-wasm/internal/global"
 )
 
 var (
 	console  = js.Global().Get("console")
-	logLevel = consoleLog
+	logLevel = LevelLog
 )
+
+const logLevelKey = "logLevel"
+
+func init() {
+	global.SetDefault(logLevelKey, LevelLog.String())
+	global.SetDefault("setLogLevel", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) < 1 {
+			return nil
+		}
+		level := args[0].String()
+		SetLevel(parseLevel(level))
+		return logLevel.String()
+	}))
+}
 
 type consoleType int
 
 const (
-	consoleDebug consoleType = iota
-	consoleLog
-	consoleWarn
-	consoleError
+	LevelDebug consoleType = iota
+	LevelLog
+	LevelWarn
+	LevelError
 )
+
+func (c consoleType) Valid() bool {
+	switch c {
+	case LevelDebug, LevelLog, LevelWarn, LevelError:
+		return true
+	default:
+		return false
+	}
+}
 
 func (c consoleType) String() string {
 	switch c {
-	case consoleDebug:
+	case LevelDebug:
 		return "debug"
-	case consoleWarn:
+	case LevelWarn:
 		return "warn"
-	case consoleError:
+	case LevelError:
 		return "error"
 	default:
 		return "log"
 	}
 }
 
+func parseLevel(level string) consoleType {
+	switch level {
+	case LevelDebug.String():
+		return LevelDebug
+	case LevelLog.String():
+		return LevelLog
+	case LevelWarn.String():
+		return LevelWarn
+	case LevelError.String():
+		return LevelError
+	default:
+		return -1
+	}
+}
+
+func SetLevel(level consoleType) {
+	if level.Valid() {
+		logLevel = level
+		global.Set(logLevelKey, logLevel.String())
+	}
+}
+
 func Debugf(format string, args ...interface{}) int {
-	return logf(consoleDebug, format, args...)
+	return logf(LevelDebug, format, args...)
 }
 
 func Printf(format string, args ...interface{}) int {
-	return logf(consoleLog, format, args...)
+	return logf(LevelLog, format, args...)
 }
 
 func Warnf(format string, args ...interface{}) int {
-	return logf(consoleWarn, format, args...)
+	return logf(LevelWarn, format, args...)
 }
 
 func Errorf(format string, args ...interface{}) int {
-	return logf(consoleError, format, args...)
+	return logf(LevelError, format, args...)
 }
 
 func logf(kind consoleType, format string, args ...interface{}) int {
@@ -58,19 +105,19 @@ func logf(kind consoleType, format string, args ...interface{}) int {
 }
 
 func Debug(args ...interface{}) int {
-	return log(consoleDebug, args...)
+	return log(LevelDebug, args...)
 }
 
 func Print(args ...interface{}) int {
-	return log(consoleLog, args...)
+	return log(LevelLog, args...)
 }
 
 func Warn(args ...interface{}) int {
-	return log(consoleWarn, args...)
+	return log(LevelWarn, args...)
 }
 
 func Error(args ...interface{}) int {
-	return log(consoleError, args...)
+	return log(LevelError, args...)
 }
 
 func log(kind consoleType, args ...interface{}) int {
@@ -83,19 +130,19 @@ func log(kind consoleType, args ...interface{}) int {
 }
 
 func DebugJSValues(args ...js.Value) int {
-	return logJSValues(consoleDebug, args...)
+	return logJSValues(LevelDebug, args...)
 }
 
 func PrintJSValues(args ...js.Value) int {
-	return logJSValues(consoleLog, args...)
+	return logJSValues(LevelLog, args...)
 }
 
 func WarnJSValues(args ...js.Value) int {
-	return logJSValues(consoleWarn, args...)
+	return logJSValues(LevelWarn, args...)
 }
 
 func ErrorJSValues(args ...js.Value) int {
-	return logJSValues(consoleError, args...)
+	return logJSValues(LevelError, args...)
 }
 
 func logJSValues(kind consoleType, args ...js.Value) int {
