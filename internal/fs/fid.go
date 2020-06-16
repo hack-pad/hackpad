3c04785fe5f67ae576a9e6184692dcec028b681a
John Starich <johnstarich@johnstarich.com>
2020-06-16 16:11:32 -0500

    Add helpful string version of FID pointers

--- internal/fs/fid.go
+++ internal/fs/fid.go
@@ -1,9 +1,19 @@
 package fs
 
-import "syscall/js"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 type FID uint64
 
 func (f FID) JSValue() js.Value {
 	return js.ValueOf(uint64(f))
+}
+
+func (f *FID) String() string {
+	if f == nil {
+		return "<nil>"
+	}
+	return fmt.Sprintf("%d", *f)
 }
