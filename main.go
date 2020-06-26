85977cc69baacfb1be6cfcb1df06e5f74d58254d
John Starich <johnstarich@johnstarich.com>
2020-06-26 00:03:09 -0500

    Replace main with polyfill command

--- main.go
+++ main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"syscall/js"
+
+	"github.com/johnstarich/go-wasm/internal/global"
+	"github.com/johnstarich/go-wasm/internal/interop"
+	"github.com/johnstarich/go-wasm/internal/js/fs"
+	"github.com/johnstarich/go-wasm/internal/js/process"
+	"github.com/johnstarich/go-wasm/log"
+)
+
 func main() {
+	process.Init()
+	fs.Init()
+	global.Set("dump", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		log.Error("Process:\n", process.Dump(), "\n\nFiles:\n", fs.Dump())
+		return nil
+	}))
+	interop.SetInitialized()
+	select {}
 }
