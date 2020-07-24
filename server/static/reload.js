46039dfe85f89a5d2e49996749b11ad08caf8ac9
John Starich <johnstarich@johnstarich.com>
2020-07-24 00:23:39 -0500

    Debounce reloads a bit

--- server/static/reload.js
+++ server/static/reload.js
@@ -1,14 +1,18 @@
 let hash = null;
 const timeoutMillis = 2000
+let changedHashRecently = false
 const poll = () => {
     fetch("/api/build").then(resp => resp.json()).then(body => {
-		const newHash = body.BuildHash;
+        const newHash = body.BuildHash;
         if (hash == null) {
             hash = newHash;
-        } else if (newHash != hash) {
-			window.location.reload()
-		}
-	})
+        } else if (newHash === hash && changedHashRecently) {
+            window.location.reload()
+        } else if (newHash !== hash) {
+            changedHashRecently = true
+            hash = newHash
+        }
+    })
 }
 poll()
 window.setInterval(poll, timeoutMillis)
