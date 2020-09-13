25e3d732d3326d476441d48ffa497ef2bb65a643
John Starich <johnstarich@johnstarich.com>
2020-09-13 18:28:30 -0500

    Auto-fit terminal to container size

--- server/src/Terminal.js
+++ server/src/Terminal.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 import 'xterm/css/xterm.css';
 import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 import { spawnTerminal, mkdirAll } from './GoWASM';
 
 export default function({ args, ...props }) {
+  const [fitAddon] = React.useState(new FitAddon())
   const [term] = React.useState(() => {
     const term = new Terminal()
+    term.loadAddon(fitAddon)
+
     const cwd = "/home/me/playground"
     mkdirAll(cwd).then(() =>
       spawnTerminal(term, { args, cwd }))
@@ -18,8 +22,10 @@ export default function({ args, ...props }) {
       term.open(elem.current)
       term.setOption('cursorBlink', true)
       term.focus()
+      fitAddon.fit()
+      window.addEventListener('resize', () => fitAddon.fit())
     }
-  }, [elem, term])
+  }, [elem, term, fitAddon])
 
   return <div ref={elem} {...props} />
 }
