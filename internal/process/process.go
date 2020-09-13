9d49c71c0c0cc35f1c1e5982e5462ac602d3963f
John Starich <johnstarich@johnstarich.com>
2020-09-12 23:22:17 -0500

    Revert "Add stdio props to JS child process object"
    This reverts commit 044b1d5b581c497a298e3428ef2319d96e64a0b2.

--- internal/process/process.go
+++ internal/process/process.go
@@ -14,10 +14,7 @@ import (
 )
 
 const (
-	minPID    = 1
-	stdinFID  = 0 // TODO should these be in 'fs'?
-	stdoutFID = 1
-	stderrFID = 2
+	minPID = 1
 )
 
 type PID = common.PID
@@ -126,18 +123,10 @@ func (p *process) SetWorkingDirectory(wd string) error {
 }
 
 func (p *process) JSValue() js.Value {
-	stdio := p.fileDescriptors.RawFIDs()
-	stdin := newWritableStream(p.ctx, stdio[stdinFID])
-	stdout := newReadableStream(p.ctx, stdio[stdoutFID], js.Null())
-	stderr := newReadableStream(p.ctx, stdio[stderrFID], js.Null())
 	return js.ValueOf(map[string]interface{}{
-		"pid":    p.pid,
-		"ppid":   p.parentPID,
-		"error":  interop.WrapAsJSError(p.err, "spawn"),
-		"stdio":  []interface{}{stdin, stdout, stderr},
-		"stdin":  stdin,
-		"stdout": stdout,
-		"stderr": stderr,
+		"pid":   p.pid,
+		"ppid":  p.parentPID,
+		"error": interop.WrapAsJSError(p.err, "spawn"),
 	})
 }
 
