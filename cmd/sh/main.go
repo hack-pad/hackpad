a45cb9e08d69f0deb128019dc6ddd32db0bd5fa1
John Starich <johnstarich@johnstarich.com>
2020-09-17 00:10:08 -0500

    Allow command passed to sh with -c

--- cmd/sh/main.go
+++ cmd/sh/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"flag"
+	"io"
 	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	color.NoColor = false // override, since wasm isn't considered a "tty"
-	reader := newRuneReader(os.Stdin)
+	command := flag.String("c", "", "Read and execute commands from the given string value.")
+	flag.Parse()
+
+	var reader io.RuneReader
+	if *command != "" {
+		reader = newRuneReader(strings.NewReader(*command))
+	} else {
+		color.NoColor = false // override, since wasm isn't considered a "tty"
+		reader = newRuneReader(os.Stdin)
+	}
 	term := newTerminal()
 
 	term.ReadEvalPrintLoop(reader)
