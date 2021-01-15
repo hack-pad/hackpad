4c279ada732f907e0691613319a5bfa1f2b92f28
John Starich <johnstarich@johnstarich.com>
2021-01-15 03:18:05 -0600

    Add more debug info to unknown errors in failed syscalls

--- internal/interop/error_js.go
+++ internal/interop/error_js.go
@@ -3,19 +3,29 @@
 package interop
 
 import (
+	"fmt"
 	"syscall/js"
 
 	"github.com/pkg/errors"
 )
 
 func WrapAsJSError(err error, message string) error {
+	return wrapAsJSError(err, message)
+}
+
+func wrapAsJSError(err error, message string, args ...js.Value) error {
 	if err == nil {
 		return nil
 	}
 
+	errMessage := errors.Wrap(err, message).Error()
+	for _, arg := range args {
+		errMessage += fmt.Sprintf("\n%v", arg)
+	}
+
 	val := js.ValueOf(map[string]interface{}{
-		"message": js.ValueOf(errors.Wrap(err, message).Error()),
-		"code":    js.ValueOf(mapToErrNo(err)),
+		"message": js.ValueOf(errMessage),
+		"code":    js.ValueOf(mapToErrNo(err, errMessage)),
 	})
 	return js.Error{Value: val}
 }
