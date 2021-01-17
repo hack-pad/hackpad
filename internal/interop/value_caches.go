6299e3625220529d60158e5fdb148a05c10aae70
John Starich <johnstarich@johnstarich.com>
2021-01-16 21:27:40 -0600

    Add IDB file path js.Value caching

---
+++ internal/interop/value_caches.go
@@ -0,0 +1,59 @@
+// +build js
+
+package interop
+
+import (
+	"syscall/js"
+)
+
+type cacher struct {
+	cache map[string]js.Value
+}
+
+func (c *cacher) value(key string, valueFn func() interface{}) js.Value {
+	if val, ok := c.cache[key]; ok {
+		return val
+	}
+	if c.cache == nil {
+		c.cache = make(map[string]js.Value)
+	}
+	val := js.ValueOf(valueFn())
+	c.cache[key] = val
+	return val
+}
+
+type StringCache struct {
+	cacher
+}
+
+func (c *StringCache) Value(s string) js.Value {
+	return c.value(s, identityStringGetter{s}.value)
+}
+
+// CallCache caches a member function by name, then runs Invoke instead of Call.
+// Has a slight performance boost, since it amortizes Reflect.get.
+type CallCache struct {
+	cacher
+}
+
+func (c *CallCache) Call(jsObj js.Value, s string, args ...interface{}) js.Value {
+	valueFn := objGetter{key: s, obj: jsObj}.value
+	return c.value(s, valueFn).Invoke(args...)
+}
+
+type identityStringGetter struct {
+	s string
+}
+
+func (i identityStringGetter) value() interface{} {
+	return i.s
+}
+
+type objGetter struct {
+	key string
+	obj js.Value
+}
+
+func (o objGetter) value() interface{} {
+	return o.obj.Get(o.key).Call("bind", o.obj)
+}
