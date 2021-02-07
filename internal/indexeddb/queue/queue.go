933b558037c002a97fef5f50402549ba6af84beb
John Starich <johnstarich@johnstarich.com>
2021-02-07 17:53:26 -0600

    Tweak IDB queue wait multiplier

--- internal/indexeddb/queue/queue.go
+++ internal/indexeddb/queue/queue.go
@@ -125,7 +125,7 @@ func (q *Queue) runDoLoop(ctx context.Context, interval time.Duration, db *index
 	prevInterval := interval
 
 	const (
-		minWaitPercent = 0.2 // must wait at least this long %-wise. needed to refill the queue
+		minWaitPercent = 1   // must wait at least this long %-wise. needed to refill the queue
 		maxWaitPercent = 100 // max wait, for responsiveness
 	)
 	minInterval := time.Duration(float64(interval) * minWaitPercent)
@@ -136,21 +136,34 @@ func (q *Queue) runDoLoop(ctx context.Context, interval time.Duration, db *index
 			timer.Stop()
 			return
 		case <-timer.C:
-			fillSize := len(q.ops)
-			_, _ = q.Do(db) // errors handled by returned channels from q.Push()
+			results, _ := q.Do(db) // errors handled by returned channels from q.Push()
 
 			/*
 				Reduce busy-wait with wait time multiplier:
 
 				use fill % to calculate next wait interval
 
-				low %:    0% filled -> 1.05-0.00 -> 1.05x prev wait
-				         25% filled -> 1.05-0.25 -> 0.80x prev wait
-				         75% filled -> 1.05-0.75 -> 0.30x prev wait
-				high %: 100% filled -> 1.05-1.00 -> 0.05x prev wait
+				low %:    0% filled -> 1.10x prev wait
+				          1% filled -> 0.50x prev wait
+				         25% filled -> 0.25x prev wait
+				         50% filled -> 0.10x prev wait
+				         75% filled -> 0.10x prev wait
+				high %: 100% filled -> 0.10x prev wait
 			*/
-			adjust := 1.05 - float64(fillSize)/maxSize
-			prevInterval = time.Duration(float64(prevInterval) * adjust)
+
+			fill := float64(len(results)) / maxSize
+			var waitMultiplier float64
+			switch {
+			case fill >= 0.50:
+				waitMultiplier = 0.10
+			case fill >= 0.25:
+				waitMultiplier = 0.25
+			case fill > 0.00:
+				waitMultiplier = 0.50
+			default:
+				waitMultiplier = 1.10
+			}
+			prevInterval = time.Duration(float64(prevInterval) * waitMultiplier)
 
 			if prevInterval < minInterval {
 				prevInterval = minInterval
