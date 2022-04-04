bd139451ac8ce74fb37ec52f41f9d1b98b2d6489
John Starich <johnstarich@johnstarich.com>
2022-04-03 22:51:35 -0500

    Add FS setup in worker, TODO reuse from main.go

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"os"
 
+	"github.com/hack-pad/go-indexeddb/idb"
 	"github.com/hack-pad/hackpad/internal/common"
+	"github.com/hack-pad/hackpad/internal/fs"
 	"github.com/hack-pad/hackpad/internal/jsworker"
 	"github.com/hack-pad/hackpad/internal/log"
 	"github.com/hack-pad/hackpad/internal/worker"
+	"github.com/hack-pad/hackpadfs/indexeddb"
 )
 
 func main() {
@@ -26,6 +29,11 @@ func main() {
 		panic(err)
 	}
 	log.Print("worker started")
+
+	if err := setUpFS(); err != nil {
+		panic(err)
+	}
+
 	<-local.Started()
 	pid := local.PID()
 	log.Print("worker process started PID ", pid)
@@ -37,4 +45,40 @@ func main() {
 	log.Warn("worker stopped for PID ", pid, "; exit code = ", exitCode)
 	local.Exit(exitCode)
 	os.Exit(exitCode)
+}
+
+func setUpFS() error {
+	const dirPerm = 0700
+	if err := os.MkdirAll("/bin", dirPerm); err != nil {
+		return err
+	}
+	if err := overlayIndexedDB("/bin", idb.DurabilityRelaxed); err != nil {
+		return err
+	}
+	if err := overlayIndexedDB("/home/me", idb.DurabilityDefault); err != nil {
+		return err
+	}
+	if err := os.MkdirAll("/home/me/.cache", dirPerm); err != nil {
+		return err
+	}
+	if err := overlayIndexedDB("/home/me/.cache", idb.DurabilityRelaxed); err != nil {
+		return err
+	}
+	if err := os.MkdirAll("/usr/local/go", dirPerm); err != nil {
+		return err
+	}
+	if err := overlayIndexedDB("/usr/local/go", idb.DurabilityRelaxed); err != nil {
+		return err
+	}
+	return nil
+}
+
+func overlayIndexedDB(mountPath string, durability idb.TransactionDurability) error {
+	idbFS, err := indexeddb.NewFS(context.Background(), mountPath, indexeddb.Options{
+		TransactionDurability: durability,
+	})
+	if err != nil {
+		return err
+	}
+	return fs.Overlay(mountPath, idbFS)
 }
